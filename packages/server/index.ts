49d5813d586ff120ba87948850c21b1bed0b5b75
ReDay Zarra <redayzarra@gmail.com>
2025-09-06 16:21:18 -0700

    Setting up OpenAI key in backend server

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,13 +1,20 @@
 import express from  "express";
 import type { Request, Response } from "express";
+import dotenv from "dotenv";
 
+// Load the .env config
+dotenv.config();
+
+// Create an express app and define the port to host the webserver
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (request, response) => {
-  response.send("Hello world!")
+// Create a basic endpoint at homepage that responds with text when visited in browser 
+app.get("/", (request: Request, response: Response) => {
+  response.send(process.env.OPENAI_API_KEY);
 })
 
+// Botts up the Express web server on the port we defined earlier
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 })
