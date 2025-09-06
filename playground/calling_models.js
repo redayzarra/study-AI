f346a141fb0c044567ea72ebafc29dd3f4b52c75
ReDay Zarra <redayzarra@gmail.com>
2025-09-05 19:17:18 -0700

    Querying the model to investigate basic API response

--- playground/calling_models.js
+++ playground/calling_models.js
@@ -1,6 +1,20 @@
 import dotenv from "dotenv";
+import { OpenAI } from "openai";
 
 // Load the .env config
 dotenv.config()
 
 const apiKey = process.env.OPENAI_API_KEY;
+
+const client = new OpenAI({
+  apiKey: apiKey
+});
+
+const response = await client.responses.create({
+  model: "gpt-3.5-turbo",
+  input: "Write a story about a garden",
+  temperature: 0.7,
+  max_output_tokens: 100
+})
+
+console.log(response);
