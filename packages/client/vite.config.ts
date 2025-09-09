48812c65836f1264ab6c883edb2730891165c4a7
ReDay Zarra <redayzarra@gmail.com>
2025-09-08 18:23:45 -0700

    Setting up tailwind css

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     // Whenever the frontend calls "/api", just forward the request to my backend server running on port:3000
     proxy: {
