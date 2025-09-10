4b0305d9b9604cc2a588bac53bae003b7ec1122b
ReDay Zarra <redayzarra@gmail.com>
2025-09-09 23:22:20 -0700

    Setting up Shadcn/UI for Vite

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     // Whenever the frontend calls "/api", just forward the request to my backend server running on port:3000
     proxy: {
