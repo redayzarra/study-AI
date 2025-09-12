4de186d5c6d5cc6870610f76f8e621a55eb935dc
ReDay Zarra <redayzarra@gmail.com>
2025-09-11 23:16:50 -0700

    Building basic backend API to interact with OpenAI models

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,24 +1,45 @@
 import express from "express";
 import type { Request, Response } from "express";
 import dotenv from "dotenv";
+import OpenAI from "openai";
 
 // Load the .env config
 dotenv.config();
 
+// Creating a new OpenAI client
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 // Create an express app and define the port to host the webserver
 const app = express();
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
 // Create a basic endpoint at homepage that responds with text when visited in browser
-app.get("/", (request: Request, response: Response) => {
-  response.send("Goodbye world.");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Goodbye world.");
 });
 
 // Create a basic endpoint at homepage that responds with text when visited in browser
-app.get("/api/hello", (request: Request, response: Response) => {
-  response.json({
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({
     message: "Hello world!",
   });
+});
+
+app.post("/api/chat", async (req: Request, res: Response) => {
+  const { prompt } = req.body;
+
+  const response = await client.responses.create({
+    model: "gpt-4o-mini",
+    input: prompt,
+    temperature: 0.1,
+    max_output_tokens: 100,
+  });
+
+  res.json({ message: response.output_text });
 });
 
 // Botts up the Express web server on the port we defined earlier
