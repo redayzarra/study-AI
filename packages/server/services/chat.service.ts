efd1bca551a5021c038205b8cfbf108f7ddbc759
ReDay Zarra <redayzarra@gmail.com>
2025-09-17 23:41:31 -0700

    Adding prompts to guide our chatbot

--- packages/server/services/chat.service.ts
+++ packages/server/services/chat.service.ts
@@ -1,8 +1,18 @@
+import fs from "fs";
+import path from "path";
 import OpenAI from "openai";
 import { conversationRepository } from "../repositories/conversation.respository";
+import template from "../prompts/chatbot.txt";
 
 // Creating a new OpenAI client
 const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+// Get the informational markdown file from prompts/ directory then add it to our chatbot prompt
+const parkInfo = fs.readFileSync(
+    path.join(__dirname, "..", "prompts", "WonderWorld.md"),
+    "utf-8"
+);
+const instructions = template.replace("{{parkInfo}}", parkInfo);
 
 interface ChatResponse {
     id: string;
@@ -17,6 +27,7 @@ export const chatService = {
     ): Promise<ChatResponse> {
         const response = await client.responses.create({
             model: "gpt-4o-mini",
+            instructions,
             input: prompt,
             temperature: 0.2,
             max_output_tokens: 200,
