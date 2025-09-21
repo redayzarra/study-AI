31ae1f0bc991d408025ec5534f90b7983877f478
ReDay Zarra <redayzarra@gmail.com>
2025-09-20 23:53:07 -0700

    Adding new controller and service for reviews

---
+++ packages/server/controllers/review.controller.ts
@@ -0,0 +1,18 @@
+import type { Request, Response } from "express";
+import { reviewService } from "../services/review.service";
+
+export const reviewController = {
+    async getReviews(req: Request, res: Response) {
+        // Get the productId from the request
+        const productId = Number(req.params.id);
+
+        // If the productId is Not a Number, then return invalid error
+        if (isNaN(productId)) {
+            res.status(400).json({ error: "Invalid productId" });
+            return;
+        }
+
+        const reviews = await reviewService.getReviews(productId);
+        res.json(reviews);
+    },
+};
--- packages/server/routes.ts
+++ packages/server/routes.ts
@@ -1,33 +1,12 @@
-import express from "express";
 import type { Request, Response } from "express";
+import express from "express";
 import { chatController } from "./controllers/chat.controller";
-import { PrismaClient } from "./generated/prisma";
+import { reviewController } from "./controllers/review.controller";
 
 const router = express.Router();
 
-// Create a basic endpoint at homepage that responds with text when visited in browser
-router.get("/", (req: Request, res: Response) => {
-    res.send("Goodbye world.");
-});
-
-// Create a basic endpoint at homepage that responds with text when visited in browser
-router.get("/api/hello", (req: Request, res: Response) => {
-    res.json({ message: "Hello world!" });
-});
-
 router.post("/api/chat", chatController.sendMessage);
 
-router.get("/api/products/:id/reviews", async (req: Request, res: Response) => {
-    const prisma = new PrismaClient();
-    const productId = Number(req.params.id);
-
-    // Prisma will generate a SQL query for us
-    const reviews = await prisma.review.findMany({
-        where: { productId },
-        orderBy: { createdAt: "desc" },
-    });
-
-    res.json(reviews);
-});
+router.get("/api/products/:id/reviews", reviewController.getReviews);
 
 export default router;
