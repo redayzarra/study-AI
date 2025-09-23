f3b0834db49bf557ccacf3b32c9b53c91856a2b2
ReDay Zarra <redayzarra@gmail.com>
2025-09-22 23:23:23 -0700

    Adding storage capability for summaries in review repository

--- packages/server/repositories/review.repository.ts
+++ packages/server/repositories/review.repository.ts
@@ -1,15 +1,33 @@
+import dayjs from "dayjs";
 import { PrismaClient, type Review } from "../generated/prisma";
+
+// Initialize a new Prisma client
+const prisma = new PrismaClient();
 
 export const reviewRepository = {
     async getReviews(productId: number, limit?: number): Promise<Review[]> {
-        // Initialize a new Prisma client
-        const prisma = new PrismaClient();
-
         // Return a list of reviews based on the productId, in descending order
         return prisma.review.findMany({
             where: { productId },
             orderBy: { createdAt: "desc" },
             take: limit,
+        });
+    },
+
+    storeReviewSummary(productId: number, summary: string) {
+        const now = new Date();
+        const expiresAt = dayjs().add(7, "days").toDate();
+        const data = {
+            content: summary,
+            expiresAt,
+            generatedAt: now,
+            productId,
+        };
+
+        prisma.summary.upsert({
+            where: { productId },
+            create: data,
+            update: data,
         });
     },
 };
